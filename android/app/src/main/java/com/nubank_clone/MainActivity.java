e469c6f952f81743ff2ae3029c627c5d01ab2329
Pardal <marcosnascimento@eng.ci.ufpb.br>
2021-08-09 11:52:10 -0300

    add gesture handler

--- android/app/src/main/java/com/nubank_clone/MainActivity.java
+++ android/app/src/main/java/com/nubank_clone/MainActivity.java
@@ -4,7 +4,6 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
-
 public class MainActivity extends ReactActivity {
 
   /**
@@ -15,7 +14,6 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "nubank_clone";
   }
-
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
     return new ReactActivityDelegate(this, getMainComponentName()) {
@@ -25,4 +23,5 @@ public class MainActivity extends ReactActivity {
       }
     };
   }
+  
 }
